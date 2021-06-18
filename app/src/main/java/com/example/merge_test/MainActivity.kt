fc2b16e3c42b20e520e10e22f64ad804106eafc4
MJZF00155619 <00155619@uca.edu.sv>
2021-06-17 23:20:30 -0700

    Agregue una linea de Mau

--- app/src/main/java/com/example/merge_test/MainActivity.kt
+++ app/src/main/java/com/example/merge_test/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("esta es mi linea, no la vayas a chingar")
+
+        var seChinga = false
+
+        if(seChinga){
+            println("te chingaste mi linea")
+        }else{
+            println("todo bien bro")
+        }
+
     }
 }
