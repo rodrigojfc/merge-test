a6c094425f4789612dca2e32609c44f3b7a7ca5e
rodrigojfc <00053714@uca.edu.sv>
2021-06-17 23:20:08 -0600

    conflict

--- app/src/main/java/com/example/merge_test/MainActivity.kt
+++ app/src/main/java/com/example/merge_test/MainActivity.kt
@@ -7,5 +7,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("Creo que empiezo a entender")
+
+
+        var a: Boolean = true
+        var b: Boolean = false
+
+        when(a) {
+            true -> b
+
+        }
+
     }
 }
